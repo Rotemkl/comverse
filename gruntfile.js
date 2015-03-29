17b6e374662e0c134b4b2dfe3347e73a3a10fa45
Rotemkl <rotemklorin@gmail.com>
2015-03-29 16:02:21 +0300

    indext.html

--- gruntfile.js
+++ gruntfile.js
@@ -11,11 +11,32 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+	
+	  md2html: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+  
+    md2html: {
+      one_file: {
+        options: {},
+        files: [{
+          src: ['index.md'],
+          dest: 'outline.html'
+        }]
+      }
     }
+	
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-md2html');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
